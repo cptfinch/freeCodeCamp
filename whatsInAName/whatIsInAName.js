f606a234be62b0913e81719e42eefec7e3cbe6c2
cptfinch <cptfinch@gmail.com>
2018-03-09 15:57:30 +0000

    doing tests

--- whatsInAName/whatIsInAName.js
+++ whatsInAName/whatIsInAName.js
@@ -9,4 +9,4 @@ function whatIsInAName(collection, source) {
     return arr;
 }
 
-exports.whatsInAName = whatsInAName;
+exports.whatIsInAName = whatIsInAName;
---
+++ whatsInAName/whatIsInAName.test.js
@@ -0,0 +1,14 @@
+const whatIsInAName = require("./whatIsInAName.js");
+
+test("whatIsInAName test 1",function(){
+    expect(whatIsInAName.whatIsInAName(
+        [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })).toEqual([{ first: "Tybalt", last: "Capulet" }])});
+
+test('whatIsInAName test 2', function(){
+    expect(whatIsInAName.whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 })).toEqual([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }])});  
+
+test('whatIsInAName test 3', function(){
+    expect(whatIsInAName.whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })).toEqual([{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }])});
+
+test('whatIsInAName test4', function(){
+    expect(whatIsInAName.whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 })).toEqual([{ "a": 1, "b": 2, "c": 2 }])}); 
